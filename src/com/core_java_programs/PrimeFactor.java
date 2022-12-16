9ff5a5ca80d5ad742bf6ed80afa5adcb8e18b42c
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 17:25:53 +0530

    UC6 finding the quotient and reminder

--- src/com/core_java_programs/PrimeFactor.java
+++ src/com/core_java_programs/PrimeFactor.java
@@ -2,38 +2,14 @@ package com.core_java_programs;
 import java.util.Scanner;
 public class PrimeFactor {
     public static void main(String args[]) {
-        System.out.println("Enter the number: ");
+        System.out.println("Enter two numbers: ");
         Scanner sc = new Scanner(System.in);
-        int y = 0;
-        int n = sc.nextInt();
-        if (n > 0)
-        {
-            for (int i = 2; i < n; i++)            //factors are only from 2 to the half of the number
-            {
-                if (n % i == 0)
-                {
-                    int x = 0;
-                    for(int j = 1; j <= i; j++)
-                    {
-                        if (i % j == 0)
-                        {
-                            x++;
-                        }
-                    }
-                    if(x == 2)
-                    {
-                        y = 1;
-                        System.out.println(i + " ");
-                    }
-                }
-            }
-            if(y == 0)
-            {
-                System.out.println("There is no prime factor for "+n);
-            }
-        }
-        else
-        {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if ((a > 0) && (b > 0)) {
+            System.out.println("The quotient is " + a / b);
+            System.out.println("The remainder is " + a % b);
+        } else {
             System.out.println("Invalud input, negative integers are not allowed");
         }
     }
