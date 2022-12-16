10134624c77d404f846e3cc2e866cb0b0e3bab03
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 17:27:06 +0530

    UC7 swapping numbers

--- src/com/core_java_programs/PrimeFactor.java
+++ src/com/core_java_programs/PrimeFactor.java
@@ -2,15 +2,18 @@ package com.core_java_programs;
 import java.util.Scanner;
 public class PrimeFactor {
     public static void main(String args[]) {
-        System.out.println("Enter two numbers: ");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if ((a > 0) && (b > 0)) {
-            System.out.println("The quotient is " + a / b);
-            System.out.println("The remainder is " + a % b);
-        } else {
-            System.out.println("Invalud input, negative integers are not allowed");
-        }
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter x=");
+        int x=sc.nextInt();
+        System.out.print("enter y=");
+        int y=sc.nextInt();
+
+        x=x+y; //30
+        y=x-y;
+        x=x-y;
+
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
     }
 }
